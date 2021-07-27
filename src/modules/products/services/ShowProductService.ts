dc45cde61122ec2fe266c2802dcdaeece7089269
Eduardo Pinheiro <eduardo.rosa@playkids.com>
2021-07-27 00:31:02 -0300

    show single product service created

---
+++ src/modules/products/services/ShowProductService.ts
@@ -0,0 +1,24 @@
+import AppError from '@shared/errors/AppError';
+import Product from '../typeorm/entities/Product';
+import { getCustomRepository } from 'typeorm';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  id: string;
+}
+
+class ListProductService {
+  public async execute({ id }: IRequest): Promise<Product | undefined> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found');
+    }
+
+    return product;
+  }
+}
+
+export default ListProductService;
