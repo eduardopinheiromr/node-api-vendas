f91076603ae9250aa31584ca520fc3779ba50ec4
Eduardo Pinheiro <3dup1n@gmail.com>
2021-07-27 16:12:37 -0300

    users fixed and tested

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,7 +1,7 @@
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
-import User from '../entities/User';
-import UsersRepository from '../repositories/UsersRepository';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
 
 interface IRequest {
   name: string;
@@ -12,11 +12,10 @@ interface IRequest {
 class CreateUserService {
   public async execute({ name, email, password }: IRequest): Promise<User> {
     const usersRepository = getCustomRepository(UsersRepository);
-
-    const emailExists = await usersRepository.findByName(email);
+    const emailExists = await usersRepository.findByEmail(email);
 
     if (emailExists) {
-      throw new AppError(`User ${email} already exists`);
+      throw new AppError('Email address already used.');
     }
 
     const user = usersRepository.create({
