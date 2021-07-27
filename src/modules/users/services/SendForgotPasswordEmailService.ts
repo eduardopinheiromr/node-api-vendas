1a37399b85f7848bdc8fb6142e38f89f29c24663
Eduardo Pinheiro <3dup1n@gmail.com>
2021-07-27 19:13:45 -0300

    feat: reset password service added

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,26 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+
+interface IRequest {
+  email: string;
+}
+
+class SendForgotPasswordEmailService {
+  public async execute({ email }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists.');
+    }
+
+    const userTokensRepository = getCustomRepository(UserTokensRepository);
+
+    await userTokensRepository.generate(user.id);
+  }
+}
+
+export default SendForgotPasswordEmailService;
