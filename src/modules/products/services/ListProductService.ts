b864e1feff86b66b377f239578d9b2b0e9e4300f
Eduardo Pinheiro <eduardo.rosa@playkids.com>
2021-07-27 00:28:16 -0300

    list product service created

---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,15 @@
+import Product from '../typeorm/entities/Product';
+import { getCustomRepository } from 'typeorm';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+class ListProductService {
+  public async execute(): Promise<Product[]> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const products = productsRepository.find();
+
+    return products;
+  }
+}
+
+export default ListProductService;
