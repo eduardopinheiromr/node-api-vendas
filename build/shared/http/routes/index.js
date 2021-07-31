24708cc9f3899ea1bbe138073d0f39e8213389be
Eduardo Pinheiro <3dup1n@gmail.com>
2021-07-31 06:14:24 -0300

    chore: production conf 1-3

--- build/shared/http/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var routes = express_1.Router();
-routes.get('/', function (req, res) {
-    res.send({ message: 'Home' });
-});
-exports.default = routes;
